1688b1b4a318aaf05bf92bc8d6544f8e6d171fac
rrebusit <rrebusit1@mail.csuchico.edu>
2023-08-22 23:14:01 -0700

    start rating analysis

--- main.py
+++ main.py
@@ -13,22 +13,49 @@ boba.shape
 #Look at variable types
 boba.info()
 
-#Remove the column *Unnamed: 0*
-del boba['Unnamed: 0']
+#In this analysis I will be removing the columns *Unnamed: 0*, *id*, *lat*, *long* 
+columns_delete = ["Unnamed: 0", "id", "lat", "long"]
+boba.drop(columns=columns_delete, inplace=True)
 boba.info()
 
-#Renaming *id* column to *shop* to make it clear it is the shop name
-boba.rename(columns={"id": "shop"}, inplace=True)
-boba.info()
+#Looking at the unique cities
+cities = boba["city"].unique()
+print(cities)
+cities_count = boba["city"].nunique()
+print(cities_count)
 
 #Looking at the count of unique values of boba shops
-unique_count = boba["shop"].nunique()
+unique_count = boba["name"].nunique()
 print(unique_count)
-unique = boba["shop"].unique()
+unique = boba["name"].unique()
 print(unique)
 
+#I can see that shops "T4 Tea" and "T4" are the same so let's make it have a uniform name
+boba["name"] = boba["name"].replace("T4", 'T4 Tea')
 
-#Looking at distribution of *rating* on *shop*
-sns.barplot(x="shop", y="rating", data=boba)
-plt.title("Bar Plot")
+#Visualize the number of boba shops in each city
+#Getting the count of each city
+shop_count = boba.groupby("city")["name"].count().reset_index(name="count")
+print(shop_count)
+
+#Looking at distribution of shops in each city
+plt.figure(figsize=(10, 14))
+sns.barplot(x="count", y="city", data=shop_count, orient="h")
+plt.title("Number of Boba Shops in Each City")
 plt.show()
+
+SF = "San Francisco"
+SF_rating = boba[boba['city'] == SF]
+SF_rating.head()
+SF_rating.shape
+
+# %%
+plt.figure(figsize=(8, 10))
+rate = sns.barplot(x="rating", y="name", data=SF_rating, orient="h")
+for p in rate.patches:
+    rating = p.get_width()
+    name = p.get_y() + p.get_height() / 2 
+    rate.annotate(f'{rating:.2f}', xy=(rating, name), xytext=(5, 0), textcoords='offset points', va='center')
+plt.title("Ratings of Boba Shops in San Francisco")
+plt.show()
+#Puppy Bobar is the best rated in San Francisco with 5 stars
