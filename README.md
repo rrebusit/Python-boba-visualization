234d8f513b573ad44d949a747557648bc33cfbf3
rrebusit <rrebusit1@mail.csuchico.edu>
2023-08-22 20:48:31 -0700

    basic eda

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # python-boba
+this is a work in progress
--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-print("hi")
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+boba = pd.read_csv("bayarea_boba_spots.csv")
+#Look at the first 5 rows
+boba.head()
+
+#Look at how many rows and how many columns
+boba.shape
+
+#Look at variable types
+boba.info()
+
+#Remove the column *Unnamed: 0*
+del boba['Unnamed: 0']
+boba.info()
+
+#Renaming *id* column to *shop* to make it clear it is the shop name
+boba.rename(columns={"id": "shop"}, inplace=True)
+boba.info()
+
+#Looking at the count of unique values of boba shops
+unique_count = boba["shop"].nunique()
+print(unique_count)
+unique = boba["shop"].unique()
+print(unique)
+
+
+#Looking at distribution of *rating* on *shop*
+sns.barplot(x="shop", y="rating", data=boba)
+plt.title("Bar Plot")
+plt.show()
